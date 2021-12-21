149c644cd0040f7d04863059521a402a2779cc47
Nitchamon <Aonoony@gmail.com>
2021-12-21 18:00:13 +0700

    "lab12_1.cpp completed"

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,34 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+    srand(time(0));
+    string grade;
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    if(rand()%9 == 0){
+        grade = "A";
+    }else if(rand()%9 == 1){
+        grade = "B+";
+    }else if(rand()%9 == 2){
+        grade = "B";
+    }else if(rand()%9 == 3){
+        grade = "C+";
+    }else if(rand()%9 == 4){
+        grade = "C";
+    }else if(rand()%9 == 5){
+        grade = "D+";
+    }else if(rand()%9 == 6){
+        grade = "D";
+    }else if(rand()%9 == 7){
+        grade = "F";
+    }else{
+        grade = "W";
+    }
+    cout << "You will get " << grade <<" in this 261102.";
+    return 0;
+}
